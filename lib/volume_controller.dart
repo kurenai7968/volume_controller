705375d57fb7f74f8cb0f8eadc6d5bbfd1e27d05
vincent <kurenai7968.dev@gmail.com>
2021-02-06 01:01:15 +0800

    update comment

--- lib/volume_controller.dart
+++ lib/volume_controller.dart
@@ -2,14 +2,22 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+/// Provide the iOS/Androd system volume.
 class VolumeController {
-  static const MethodChannel _methodChannel = MethodChannel('com.kurenai7968.volume_controller.method');
-  static const EventChannel _eventChannel = EventChannel('com.kurenai7968.volume_controller.volume_listener_event');
+  /// This method channel is used to communicate with iOS/Android method.
+  static const MethodChannel _methodChannel =
+      MethodChannel('com.kurenai7968.volume_controller.method');
 
+  /// This event channel is used to communicate with iOS/Android event.
+  static const EventChannel _eventChannel =
+      EventChannel('com.kurenai7968.volume_controller.volume_listener_event');
+
+  /// This method listen to the system volume. The volume value will be generated when the volume was changed.
   static Stream<double> get volumeListener {
     return _eventChannel.receiveBroadcastStream().map((d) => d as double);
   }
 
+  /// This method set the system volume between 0.0 to 1.0.
   static void setVolume(double volume) {
     if (volume != null) {
       _methodChannel.invokeMethod('setVolume', {"volume": volume});
@@ -18,14 +26,17 @@ class VolumeController {
     }
   }
 
+  /// This method set the system volume to max.
   static void maxVolume() {
     _methodChannel.invokeMethod('setVolume', {"volume": 1.0});
   }
 
+  /// This method mute the system volume that mean the volume set to min.
   static void muteVolume() {
     _methodChannel.invokeMethod('setVolume', {"volume": 0.0});
   }
 
+  /// This method get the current system volume.
   static Future<double> getVolume() async {
     return await _methodChannel.invokeMethod('getVolume');
   }
